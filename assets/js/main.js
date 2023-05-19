aaab1210c3d71d7fc684c61f404fc9852200525d
freekweb <133934536+freekweb@users.noreply.github.com>
2023-05-19 22:40:43 +0800

    Add files via upload
    materialize sample ebooks

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
+    // $('.carousel.carousel-slider').carousel({
+    //     fullWidth: true,
+    //     indicators: true,
+    //     noWrap: false
+    // });
+    $('.slider').slider();
+});
